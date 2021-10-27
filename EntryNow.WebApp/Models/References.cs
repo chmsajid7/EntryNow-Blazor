052a9bb3429360861b22a3a5905502cc00492e39
hp <hp@DESKTOP-NC0MAAO>
2021-10-28 00:53:28 +0500

    -- references grid

---
+++ EntryNow.WebApp/Models/References.cs
@@ -0,0 +1,21 @@
+﻿namespace EntryNow.WebApp.Models
+{
+    public class References
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ContactNumber { get; set; }
+        public string CNIC { get; set; }
+        public string ReferencedBy { get; set; }
+        public int Recommended { get; set; }
+        public int? ReferenceId { get; set; }
+        public string UnionCounsil { get; set; }
+        public string Taluka { get; set; }
+        public string District { get; set; }
+        public string Deh { get; set; }
+        public string Village { get; set; }
+        public string City { get; set; }
+        public string Address { get; set; }
+    }
+}
