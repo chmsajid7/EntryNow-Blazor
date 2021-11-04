8122623c8a1e8eaa29075a59804c146dad44149e
hp <hp@DESKTOP-NC0MAAO>
2021-11-04 06:52:26 +0500

    -- fix

---
+++ EntryNow.WebApp/Models/DropDownSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EntryNow.WebApp.Models
+{
+    public class DropDownSettings
+    {
+        public int Id { get; set; }
+        public string SurnameName { get; set; }
+    }
+    //public class DropDownSettings_ViewModel
+    //{
+    //    public int Id { get; set; }
+    //    [Required]
+    //    public string SurnameName { get; set; }
+    //}
+}
