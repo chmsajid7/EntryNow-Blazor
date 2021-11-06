5416a88ab95e8293ce57c0db57d9aca97efbc075
hp <hp@DESKTOP-NC0MAAO>
2021-11-06 17:42:31 +0500

    --fix

--- EntryNow.WebApp/Models/DropDownSettings.cs
+++ EntryNow.WebApp/Models/DropDownSettings.cs
@@ -7,6 +7,17 @@ namespace EntryNow.WebApp.Models
         public int Id { get; set; }
         public string SurnameName { get; set; }
     }
+    public class DropDownSettings_UC
+    {
+        public int Id { get; set; }
+        public string UnionCounsilName { get; set; }
+    }
+    public class DropDownSettings_Village
+    {
+        public int Id { get; set; }
+        public string VillageName { get; set; }
+    }
+
     //public class DropDownSettings_ViewModel
     //{
     //    public int Id { get; set; }
