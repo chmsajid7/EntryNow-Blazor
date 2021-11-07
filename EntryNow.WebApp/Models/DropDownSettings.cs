c406efdd67587ff1491732e5658cc3f629567ded
hp <hp@DESKTOP-NC0MAAO>
2021-11-07 05:44:06 +0500

    --

--- EntryNow.WebApp/Models/DropDownSettings.cs
+++ EntryNow.WebApp/Models/DropDownSettings.cs
@@ -5,16 +5,19 @@ namespace EntryNow.WebApp.Models
     public class DropDownSettings
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter Surname")]
         public string SurnameName { get; set; }
     }
     public class DropDownSettings_UC
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter Union Council")]
         public string UnionCounsilName { get; set; }
     }
     public class DropDownSettings_Village
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter Village")]
         public string VillageName { get; set; }
     }
 
