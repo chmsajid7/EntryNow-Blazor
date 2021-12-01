4cb9b0aa01a92ecad0c6052c1bc8dfa7e958afb8
hp <hp@DESKTOP-NC0MAAO>
2021-12-02 01:23:27 +0500

    --

--- EntryNow.Web/Models/References.cs
+++ EntryNow.Web/Models/References.cs
@@ -4,17 +4,22 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public int? SurnameId { get; set; }
         public string Surname { get; set; }
         public string ContactNumber { get; set; }
         public string CNIC { get; set; }
         public string ReferencedBy { get; set; }
         public int Recommended { get; set; }
         public int? ReferenceId { get; set; }
+        public int? UnionCounsilId { get; set; }
         public string UnionCounsil { get; set; }
+        public int? TalukaId { get; set; }
         public string Taluka { get; set; }
         public string District { get; set; }
         public string Deh { get; set; }
+        public int? VillageId { get; set; }
         public string Village { get; set; }
+        public int? CityId { get; set; }
         public string City { get; set; }
         public string Address { get; set; }
     }
